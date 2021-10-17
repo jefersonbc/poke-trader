4c4fd14d3d14fe9b49377fa43c1cdf76c1d014e5
Jeferson Brito <nashbox@gmail.com>
2021-10-17 13:06:23 -0300

    Adicionando model Pokemon; Adicionando seed para popular os pokemons através da API PokeAPI.

---
+++ db/migrate/20211017155658_create_pokemons.rb
@@ -0,0 +1,11 @@
+class CreatePokemons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :base_experience
+      t.string :sprite
+
+      t.timestamps
+    end
+  end
+end
