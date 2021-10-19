62c5ddf649a741e49c3dc5214d29a47dc7f50658
Jeferson Brito <nashbox@gmail.com>
2021-10-19 16:34:42 -0300

    Add methods to return total xp for each player and a method to return is_far as string

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -13,6 +13,17 @@ class Trade < ApplicationRecord
         return player_one_pokemons
     end
 
+    def player_one_pokemons_total_xp
+        player_one_total_xp = 0
+        
+        trade_items = self.trade_items.where(player_id: self.player_one_id)
+        trade_items.each do |item|
+            player_one_total_xp += item.pokemon.base_experience
+        end
+        
+        return player_one_total_xp
+    end
+
     def player_two_pokemons
         player_two_pokemons = []
         
@@ -22,5 +33,25 @@ class Trade < ApplicationRecord
         end
         
         return player_two_pokemons
+    end
+
+    def player_two_pokemons_total_xp
+        player_two_total_xp = 0
+        
+        trade_items = self.trade_items.where(player_id: self.player_two_id)
+        trade_items.each do |item|
+            player_two_total_xp += item.pokemon.base_experience
+        end
+        
+        return player_two_total_xp
+    end
+
+    def is_fair_to_s
+        case self.is_fair
+            when false
+            "INJUSTA"
+            else
+            "JUSTA"
+        end
     end
 end
