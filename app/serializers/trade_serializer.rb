ac05d5a4f308a44cad0dea7098cc7277e2c55608
Jeferson Brito <nashbox@gmail.com>
2021-10-18 23:12:37 -0300

    Add route to Trade API.

--- app/serializers/trade_serializer.rb
+++ app/serializers/trade_serializer.rb
@@ -1,4 +1,4 @@
 class TradeSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :is_fair, :player_one_id, :player_two_id
-  end
+    attributes :is_fair, :player_one_id, :player_two_id, :trade_items
+end
